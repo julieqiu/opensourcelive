58aefd214252bef945e7eeea1fb5569e583c6b71
Rebecca Stambler <rstambler@golang.org>
2020-10-01 00:02:51 -0400

    presentation starting point

--- go.mod
+++ go.mod
@@ -1,10 +1,3 @@
 module github.com/julieqiu/opensourcelive
 
 go 1.15
-
-require (
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/russross/blackfriday/v2 v2.0.1
-	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
-	github.com/yuin/goldmark v1.2.1
-)
--- main.go
+++ main.go
@@ -2,80 +2,15 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"strings"
-
-	"html/template"
-
-	"github.com/russross/blackfriday/v2"
-	"github.com/yuin/goldmark"
-	"github.com/yuin/goldmark/extension"
-	"github.com/yuin/goldmark/renderer/html"
-	"github.com/yuin/goldmark/text"
 )
 
-type Page struct {
-	Body template.HTML
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
 func main() {
 	http.HandleFunc("/", handler)
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
-
-	addr := ":8080"
-	log.Printf("Listening on addr %s", addr)
-	log.Fatal(http.ListenAndServe(addr, nil))
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/" {
-		fmt.Fprintf(w, "Hello! Type the recipe you are looking for into the URL.")
-		return
-	}
-	title := strings.TrimPrefix(r.URL.Path, "/")
-	p, err := loadPage(title)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	tmpl := template.Must(template.New("view").Parse(`<div>{{.Body}}</div>`))
-
-	if err := tmpl.Execute(w, p); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-func loadPage(title string) (*Page, error) {
-	filename := title + ".md"
-	body, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-
-	b := renderBlackfriday(body)
-	if err != nil {
-		return nil, err
-	}
-	return &Page{Body: b}, nil
-}
-
-func renderBlackfriday(body []byte) template.HTML {
-	md := blackfriday.Run(body)
-	return template.HTML(md)
-}
-
-func renderMarkdown(input []byte) (template.HTML, error) {
-	md := goldmark.New(
-		goldmark.WithExtensions(extension.GFM, extension.Table),
-		goldmark.WithRendererOptions(html.WithUnsafe()))
-	reader := text.NewReader(input)
-	doc := md.Parser().Parse(reader)
-	var b strings.Builder
-	if err := md.Renderer().Render(&b, input, doc); err != nil {
-		return "", err
-	}
-	return template.HTML(b.String()), nil
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
