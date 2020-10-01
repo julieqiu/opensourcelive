f523cbf31e99815a44bf43c3bd5284401b7deae2
Rebecca Stambler <rstambler@golang.org>
2020-10-01 00:09:29 -0400

    reorder code

--- main.go
+++ main.go
@@ -6,11 +6,11 @@ import (
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
-}
-
 func main() {
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
