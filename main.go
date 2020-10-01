5ae146a3b0d9f76442b7caec2010a7201fca66f3
Rebecca Stambler <rstambler@golang.org>
2020-10-01 00:29:14 -0400

    switch to command line

--- main.go
+++ main.go
@@ -1,23 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-)
+import "io/ioutil"
 
 type Page struct {
 	Body string
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	loadPage("static/turkeychili.md")
 }
 
 func loadPage(title string) (*Page, error) {
