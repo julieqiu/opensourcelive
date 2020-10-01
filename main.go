b836c1e369f74c9ca8fda2ab69f72b80b30cc278
Rebecca Stambler <rstambler@golang.org>
2020-10-01 00:27:12 -0400

    just use a basic main

--- main.go
+++ main.go
@@ -1,16 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-)
-
 func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
