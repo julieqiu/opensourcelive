6b04dc678cf8ed8f208e4411bb4af20f91ba4058
Rebecca Stambler <rstambler@golang.org>
2020-10-01 00:21:14 -0400

    add handler

--- main.go
+++ main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 )
 
 type Page struct {
-	Body []byte
+	Body string
 }
 
 func main() {
@@ -17,7 +19,22 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	if r.URL.Path == "/" {
+		fmt.Fprintf(w, "Hello! Type the recipe you are looking for into the URL.")
+		return
+	}
+	title := strings.TrimPrefix(r.URL.Path, "/")
+	p, err := loadPage(title)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	tmpl := template.Must(template.New("view").Parse(`<div>{{.Body}}</div>`))
+
+	if err := tmpl.Execute(w, p); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func loadPage(title string) (*Page, error) {
@@ -26,5 +43,5 @@ func loadPage(title string) (*Page, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &Page{Body: body}, nil
+	return &Page{Body: string(body)}, nil
 }
