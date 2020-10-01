2f6dff443f86f5981bb2c3c04008299acf73b443
Rebecca Stambler <rstambler@golang.org>
2020-10-01 00:35:54 -0400

    add basic web server

--- main.go
+++ main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"strings"
 )
 
 type Page struct {
@@ -19,22 +17,7 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/" {
-		fmt.Fprintf(w, "Hello! Type the recipe you are looking for into the URL.")
-		return
-	}
-	title := strings.TrimPrefix(r.URL.Path, "/")
-	p, err := loadPage(title)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	tmpl := template.Must(template.New("view").Parse(`<div>{{.Body}}</div>`))
-
-	if err := tmpl.Execute(w, p); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
 func loadPage(title string) (*Page, error) {
