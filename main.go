6fde1ba5298917239e617d2b0201290b8362151c
Rebecca Stambler <rstambler@golang.org>
2020-10-01 23:40:36 -0400

    update to latest version

--- main.go
+++ main.go
@@ -9,39 +9,31 @@ import (
 	"strings"
 )
 
-type Page struct {
-	Body string
-}
-
 func main() {
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/" {
-		fmt.Fprintf(w, "Hello! Type the recipe you are looking for into the URL.")
+	title := strings.TrimPrefix(r.URL.Path, "/")
+	if title == "" {
+		fmt.Fprintf(w, "Please type the item you want to search for in the URL.")
 		return
 	}
-	title := strings.TrimPrefix(r.URL.Path, "/")
 	p, err := loadPage(title)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
 	}
-
-	tmpl := template.Must(template.New("view").Parse(`<div>{{.Body}}</div>`))
-
+	tmpl := template.Must(template.New("view").Parse(`<div>{{.}}</div>`))
 	if err := tmpl.Execute(w, p); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
-func loadPage(title string) (*Page, error) {
-	filename := title + ".md"
+func loadPage(filename string) (string, error) {
 	body, err := ioutil.ReadFile(filename)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
-	return &Page{Body: string(body)}, nil
+	return string(body), nil
 }
